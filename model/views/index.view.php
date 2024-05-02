632ecd4e56cec17fb8439bfc27bdb0b050bc8933
Andres2600 <129902164+Andres2600@users.noreply.github.com>
2024-05-01 20:40:48 -0400

    change to CSS, add Js File

--- model/views/index.view.php
+++ model/views/index.view.php
@@ -1,3 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="Csympa">
+    <meta name="description" content="this is my personal blog website"> 
+
+     <!-- google font link -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap"rel="stylesheet">
+ 
+       <!-- CSS link -->
+       <link rel="stylesheet" href="css/style.css"/>
+       
+    <!-- logo -->
+  <link rel="shorcut icon" href="img/default.png">
+
+  <!-- preload image -->
+  <link rel="preload" as="image"  href="/public/img/hero-banner.jpg">
+  <link rel="preload" as="image"  href="/public/img/pattern-1.svg">
+  <link rel="preload" as="image"  href="/public/img/pattern-2.svg">
+
+    <!-- Title -->
+    <title>Csympa</title>
+  </head>
+  <body>
+ 
+<header class="header" data-header>
+            <div class="container">
+              <a href="#" class="logo">
+                <img src="img/default.png" alt="Csympa logo" width="50" height="37">
+              </a>
+              <nav class="navbar" data-navbar>  
+
+                    <div class="navbar-top">
+                      <a href="#" class="logo">
+                      <img src="img/default.png" alt="Csympa logo" width="119" height="37">
+                      </a>
+                      <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
+                        <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+                    </button>
+                  </div>
+
+
+                  <ul class="navbar-list">
+                    <li>
+                      <a href="#home" class="navbar-link hover-1" data-nav-toggler>Accueil</a>
+                    </li>
+
+                    <li>
+                      <a href="#topics" class="navbar-link hover-1" data-nav-toggler>Textes</a>
+                    </li>
+
+                    <li>
+                      <a href="#featured" class="navbar-link hover-1" data-nav-toggler>post annexe</a>
+                    </li>
+
+                    <li>
+                      <a href="#recent" class="navbar-link hover-1" data-nav-toggler>Post recent</a>
+                    </li>
+
+                    <li>
+                      <a href="#" class="navbar-link hover-1" data-nav-toggler>Contacts</a>
+                    </li>
+                  </ul>
+
+                  <div class="navbar-bottom">
+                    <div class="profile-card">
+                      <img src="/img/profile-pic.png" width="48" height="48" alt="Andrès" class="profile-banner">
+                      <div>
+                        <p class="card-title"> Salut Andrès !</p>
+
+                        <p class="card-subtitle">
+                          Vous Avez 3 messages!
+                        </p>
+                      </div>
+                    </div>
+
+                    <ul class="link-list">
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">Profil</a>
+                      </li>
+
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">Articles Enregistrés</a>
+                      </li>
+
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">faire un nouveau post</a>
+                      </li>
+
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">Mes j'aime</a>
+                      </li>
+
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">Paramètres</a>
+                      </li>
+
+                      <li>
+                        <a href="#" class="navbar-bottom-link hover-1">Deconnecter</a>
+                      </li>
+                    </ul>  
+                  
+                  </div>
+
+                  <p class="copyright-text">
+                      Copyright 2022  Csympa - Personal Blog Template Developed by codewithsadee
+                    </p>
+              </nav>
+
+              <a href="#" class="btn btn-primary"> S'inscrire</a>
+              <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+                <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+            </button>
+            </div>
+</header>
+
+<main>
+  <article>
+    <!-- HERO -->
+
+    <section class="hero" id="home" aria-label="home">
+      <div class="container">
+
+        <div class="hero-content">
+          <p class="hero-subtitle"> Salut tout le monde!</p> 
+
+            <h1 class="headline headline-1 section-title">Je suis<span class="span">Andrès S. Saintil</span></h1>
+
+            <p class="hero-text">
+              Vous êtes sur mon blog Personnel, entant qu'informaticien et community manager je vous invite à vous abonné et à recevoir,
+              des articles intéressant sur des sujets pertinent qui vous intéresse.
+          </p>
+
+          <div class="input-wrapper">
+            <input type="email" name="email-adress" placeholder="tapez votre adresse email" required 
+            class="input-field">
+
+            <button class="btn btn-primary">
+              <span class="span">S'inscrire</span>
+              <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+            </button>
+          </div>
+
+        </div>
+
+        <div class="hero-banner">
+          <img src="/img/hero-banner.jpg" width="327" height="490" alt="Andrès S. Saintil" class="w-100">
+          <img src="/public/img/pattern-2.svg" width="27" height="26" alt="shape" class="shape shape-1">
+          <img src="/public/img/pattern-3.svg" width="27" height="26" alt="shape" class="shape shape-2">
+
+        </div>
+
+      </div>
+
+    </section>
+  </article>
+</main>
+    
+
+<!-- js link -->
+<script src="js/script.js"></script>
+
+
+<!-- ionicon link -->
+<script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
+<script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+     
+</body>
+</html>
+
+
+
+
 <?php
-     echo'<h1> Home </h1>';
+     echo'<h1>home  </h1>';
      ?>
--- model/views/layouts/default.view.layout.php
+++ model/views/layouts/default.view.layout.php
@@ -1,143 +1,16 @@
+
+
+
+
+
+
+
+
+
+
 <!doctype html>
 <html lang="en">
-  <head>
-
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- CSS link -->
-    <link rel="stylesheet" href="css/style.css"/>
-  <link rel="shorcut icon" href="img/default.png">
-    <title>Index | Csympa</title>
-  </head>
-  <body>
-
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent shadow">
-        <div class="container-fluid">
-
-            <!-- LOGO -->
-          <a class="navbar-brand" href="#"><img src="img/default-monochrome.svg" alt="" width="150" leight="60"></a>
-
-          <!--toggle Btn -->
-          <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <!-- Sidebar -->
-          <div class=" sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            
-            <!-- Sidebar Header -->
-            <div class="offcanvas-header text-white border-bottom">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-              <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <!-- Sidebar Body -->
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="http://andrs.blog/">Marketing</a>
-                </li>
-
-                <li class="nav-item mx-2">
-                  <a class="nav-link" href="http://andrs.blog/contact">Technologie</a>
-                </li>
-                <li class="nav-item mx-2">
-                    <a class="nav-link" href="#">Competence</a>
-                  </li>
-                <li class="nav-item mx-2">
-                    <a class="nav-link" href="#"> A propos</a>
-                </li>
-            </ul>
-            <!-- Login/ Sign up -->
-            <div class="d-flex justify-content-center align-items-center gap-3">
-              <a href="#login" class="text-white">Login</a>
-              <a href="#signup" class="text-white text-decoration-none px-3 py-1 bg-primary rounded-4" style="background-color: #4ce5f9">Sign Up</a>
-            </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    <section id="article">
-
-    <!-- heading -->
-    <div class="blog-heading">
-      <span> 
-        <h1>Marketing</h1> 
-     </span>
-      <h2> <i>Des contenus pertinents pour les professionnels du marketing digital: <br> nouveautés,guide et conseils. </i></h2>
-    </div>
- <!-- articles container -->
- <div class="article-container">
-    <!-- box 1 -->
-    <div class="article-box">
-      <!-- img -->
-      <div class="article-img">
-        <img src="img/instagram.jpg" alt="instagram" width="350" height="250">
-      </div>
-    </div>
-  </div>
-    </section>
- 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
- 
-  <main role="main">
-    <?=$pageContent?> 
-  </main>
-
+   
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
@@ -145,7 +18,9 @@
   </body>
 </html>
 
-
+<main role="main">
+    <?=$pageContent?> 
+  </main>
 
 <!-- <header>
          <nav class="navbar navbar-expand.lg navbar-dark bg-dark shadow">
